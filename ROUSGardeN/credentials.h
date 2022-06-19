c96a1a271bcb9c83985db9e42ae8245f1d3a41df
[ ıı ] [  ] [ ■ ] <59439463+txomin-guardia@users.noreply.github.com>
2022-06-19 11:33:57 +0200

    basic credentiasl.h file

---
+++ ROUSGardeN/credentials.h
@@ -0,0 +1,8 @@
+// WiFi authentication info.
+char my_SSID[] = ""
+char my_PASSWORD[] = ""
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = ""
+char password[] = ""
+char clientID[] = ""

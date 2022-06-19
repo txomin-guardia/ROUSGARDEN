069c35e9d21ba9aac6a4d7f861b2331b28ac81e9
[ ıı ] [  ] [ ■ ] <59439463+txomin-guardia@users.noreply.github.com>
2022-06-19 12:20:23 +0200

    Delete credentials.h

--- ROUSGardeN/credentials.h
+++
@@ -1,8 +0,0 @@
-// WiFi authentication info.
-char my_SSID[] = ""
-char my_PASSWORD[] = ""
-
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = ""
-char password[] = ""
-char clientID[] = ""
